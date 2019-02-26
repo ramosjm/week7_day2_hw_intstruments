01cf97c18acc24f05f024bd6048f095d7f5f08c6
ramosjm <jjramos_2004@hotmail.com>
2019-02-26 16:56:49 +0000

    added helpers directory.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
+const instrumentFamilyData = require('./data/instrument_family_data.js');
+
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
+  const dropdownElement = document.querySelector('#instrument-families');
+  const instrumentDropdown = new SelectView(dropdownElement);
+  instrumentDropdown.bindEvents();
+
 });
